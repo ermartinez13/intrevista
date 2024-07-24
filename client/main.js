575d90f669ffc830d3e4c0713950a09d04a5403e
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-07-24 18:58:22 +0000

    feat: show webcam stream on start button click

--- client/main.js
+++ client/main.js
@@ -1,1 +1,30 @@
-import "./style.css"
+import "./style.css";
+
+const videoElem = document.getElementById("video");
+const startBtn = document.getElementById("startButton");
+const stopBtn = document.getElementById("stopButton");
+
+startBtn.addEventListener("click", handleStart);
+
+function handleStart() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: true,
+    })
+    .then(showInputStream)
+    .catch((error) => {
+      if (error.name === "NotFoundError") {
+        console.error("Camera or microphone not found. Can't record.");
+      } else {
+        console.error(error);
+      }
+    });
+}
+
+function showInputStream(stream) {
+  videoElem.srcObject = stream;
+  videoElem.captureStream =
+    videoElem.captureStream || videoElem.mozCaptureStream;
+  return videoElem.play();
+}
