5712c6449c6ddb81e875088f954ca45226680715
Erick Martinez <60302538+ermartinez13@users.noreply.github.com>
2024-08-01 15:05:30 -0400

    INT-12: Persist Data to IndexedDB (#3)
    * feat: persist video to indexeddb
    
    * retrieve and enable playback of saved recordings
    
    * use kebab-casing for element class names and ids
    
    * refactor: rename and create function abstractions
    
    * unmute video when playback src is set
    
    * update recordings list on save
    
    * create metadata object store
    
    * save video metadata to object store
    
    * render metadata
    
    * retrieve video blob and set as video source

--- client/main.js
+++ client/main.js
@@ -1,16 +1,31 @@
 import "./style.css";
 
 const videoElem = document.getElementById("video");
-const startBtn = document.getElementById("startButton");
-const stopBtn = document.getElementById("stopButton");
+const startBtn = document.getElementById("start-button");
+const stopBtn = document.getElementById("stop-button");
+const saveBtn = document.getElementById("save-button");
+const recordingList = document.getElementById("recordings");
 
 let data = [];
 let mediaRecorder = null;
+let db = null;
 
-startBtn.addEventListener("click", handleStart);
-stopBtn.addEventListener("click", handleStop);
+const DB_NAME = "IntrevistaDB";
+const DB_VERSION = 2;
+const VIDEO_STORE = "videos";
+const METADATA_STORE = "metadata";
 
-function handleStart() {
+const dbRequest = indexedDB.open(DB_NAME, DB_VERSION);
+
+startBtn.addEventListener("click", handleStartRecording);
+stopBtn.addEventListener("click", handleStopRecording);
+dbRequest.addEventListener("error", console.error);
+dbRequest.addEventListener("success", handleDBConnection);
+dbRequest.addEventListener("upgradeneeded", handleDBUpgrade);
+saveBtn.addEventListener("click", handleSaveRecording);
+recordingList.addEventListener("click", setPlaybackSource);
+
+function handleStartRecording() {
   navigator.mediaDevices
     .getUserMedia({
       video: true,
@@ -38,23 +53,24 @@ function showInputStream(stream) {
 
 function startRecording() {
   data = []; // reset data
+  saveBtn.setAttribute("disabled", "");
   const stream = videoElem.captureStream();
   mediaRecorder = new MediaRecorder(stream, {
     mimeType: "video/webm",
   });
   mediaRecorder.ondataavailable = (event) => data.push(event.data);
   mediaRecorder.onerror = (error) => console.error("Recording Error: ", error);
+  mediaRecorder.onstop = enableRecordingPlayback;
   mediaRecorder.start();
 }
 
-function handleStop() {
+function handleStopRecording() {
   videoElem.srcObject.getTracks().forEach((track) => track.stop());
   mediaRecorder.stop();
   mediaRecorder = null;
   videoElem.srcObject = null;
   startBtn.removeAttribute("disabled");
   stopBtn.setAttribute("disabled", "");
-  setTimeout(enableRecordingPlayback, 0); // allows processing of MediaRecorder's datavailable event
 }
 
 function enableRecordingPlayback() {
@@ -62,4 +78,103 @@ function enableRecordingPlayback() {
   videoElem.src = URL.createObjectURL(recordingBlob);
   videoElem.setAttribute("controls", "");
   videoElem.muted = false;
+  saveBtn.removeAttribute("disabled");
+}
+
+function handleSaveRecording() {
+  if (db) {
+    const recordingBlob = new Blob(data, { type: "video/webm" });
+    const transaction = db.transaction(
+      [VIDEO_STORE, METADATA_STORE],
+      "readwrite"
+    );
+    const objectStore = transaction.objectStore(VIDEO_STORE);
+    const request = objectStore.add(recordingBlob);
+    request.onsuccess = (event) => {
+      const videoKey = event.target.result;
+      const timestamp = new Date();
+      const metadataStore = transaction.objectStore(METADATA_STORE);
+      const metadata = {
+        videoKey,
+        createdAt: timestamp,
+        description: "",
+        title: "untitled",
+        updatedAt: timestamp,
+      };
+      const metadataRequest = metadataStore.add(metadata);
+      metadataRequest.onerror = (event) => {
+        console.error("Metadata save error: ", event.target.error);
+      };
+      saveBtn.setAttribute("disabled", "");
+      saveBtn.textContent = "Saved!";
+      metadataRequest.onsuccess = () => appendMetadataToList(metadata);
+    };
+  }
+}
+
+function getMetadataList(successHandler) {
+  const transaction = db.transaction([METADATA_STORE], "readonly");
+
+  transaction.onerror = (event) => {
+    console.error("Metadata retrieval transaction error: ", event.target.error);
+  };
+
+  const metadataStore = transaction.objectStore(METADATA_STORE);
+  const request = metadataStore.getAll();
+
+  request.onsuccess = (event) => {
+    const metadataList = event.target.result;
+    successHandler(metadataList);
+  };
+}
+
+function renderMetadata(metadataList) {
+  metadataList.forEach((metadata) => {
+    appendMetadataToList(metadata);
+  });
+}
+
+function appendMetadataToList(metadata) {
+  const listItemElem = document.createElement("li");
+  const buttonElem = document.createElement("button");
+  const paragraphElem = document.createElement("p");
+  buttonElem.textContent = `Play ${metadata.title}`;
+  paragraphElem.textContent = `Recorded on: ${metadata.createdAt}`;
+  buttonElem.setAttribute("data-video-key", metadata.videoKey);
+  listItemElem.appendChild(paragraphElem);
+  listItemElem.appendChild(buttonElem);
+  recordingList.appendChild(listItemElem);
+}
+
+function setPlaybackSource(event) {
+  const targetElem = event.target;
+  if (targetElem.tagName === "BUTTON") {
+    const videoKey = targetElem.getAttribute("data-video-key");
+    const transaction = db.transaction(VIDEO_STORE, "readonly");
+    const request = transaction.objectStore(VIDEO_STORE).get(Number(videoKey));
+    request.onsuccess = (event) => {
+      const recordingBlob = event.target.result;
+      const recordingURL = URL.createObjectURL(recordingBlob);
+      videoElem.src = recordingURL;
+      videoElem.setAttribute("controls", "");
+      videoElem.muted = false;
+    };
+  }
+}
+
+function handleDBUpgrade(event) {
+  const db = event.target.result;
+  const storeNames = db.objectStoreNames;
+
+  if (!storeNames.contains(VIDEO_STORE)) {
+    db.createObjectStore(VIDEO_STORE, { autoIncrement: true });
+  }
+  if (!storeNames.contains(METADATA_STORE)) {
+    db.createObjectStore(METADATA_STORE, { autoIncrement: true });
+  }
+}
+
+function handleDBConnection(event) {
+  db = event.target.result;
+  getMetadataList(renderMetadata);
 }
